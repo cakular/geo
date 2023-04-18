706e97de5dec4de8a9171ccdcbd7806e24921049
hemisemidemipresent <hnngggrrrr@gmail.com>
2023-04-18 19:09:05 +0800

    kaggle + geoguessr

--- aaaaaaa.js
+++ aaaaaaa.js
@@ -1,14 +1,5 @@
-const amogus = require('./amogus.json');
-const fs = require('fs');
-amogus.map((obj) => {
-    if (!obj) return;
-    obj.lat = obj.coordinates[1];
-    obj.lng = obj.coordinates[0];
-    delete obj.coordinates;
+const amogus = require('./amogus2.json');
 
-    obj.location_id = `${roundto10(obj.lat + 90).padStart(3, '0')}${roundto10(obj.lng + 180).padStart(3, '0')}`;
-});
-fs.writeFileSync(`./amogus2.json`, JSON.stringify(amogus));
-function roundto10(i) {
-    return Math.floor(i / 10) * 10 + '';
-}
+let i = amogus.find((e) => e?.id == 148491553920492);
+
+console.log(i);
